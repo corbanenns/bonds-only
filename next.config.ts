2edc766d86752041c195f4ae41cf24ff236e6e32
corbanenns <corban@comharins.com>
2025-11-07 17:53:17 -0800

    Add Vercel Blob storage to allowed image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'iaxlywoszs4tdq1y.public.blob.vercel-storage.com',
+        pathname: '/profiles/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
